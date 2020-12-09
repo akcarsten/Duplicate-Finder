2c41a4b574362b8b1de3cc55860e9cf364b084c3
Carsten Klein <carsten.a.klein@gmail.com>
2020-12-09 23:24:44 +0100

    clean up

--- Duplicates/Duplicates.py
+++ Duplicates/Duplicates.py
@@ -8,6 +8,12 @@ class Duplicates:
     def __init__(self):
         self.hash = []
 
+    @staticmethod
+    def filelist(filepath):
+
+        files = ['{}\\{}'.format(filepath, x) for x in os.listdir(filepath)]
+        return files
+
     def hashfile(self, file, blocksize=65536):
 
         afile = open(file, 'rb')
@@ -21,45 +27,11 @@ class Duplicates:
 
         return self.hash
 
-    @staticmethod
-    def filelist(filepath):
-
-        files = ['{}\\{}'.format(filepath, x) for x in os.listdir(filepath)]
-        return files
-
     def hashtable(self, files):
-        print(files)
+
         hash = []
         for file in files:
                 print(file, end='\r')
                 hash.extend([self.hashfile(file)])
 
         return hash
-
-'''
-path = 'C:/Users/carst/Google Drive'
-csv_output = 'C:/Users/carst/duplicates.csv'
-csv_output2 = 'C:/Users/carst/only_duplicates.csv'
-
-# Calculate checksum of each file
-checksum = []
-for file in full_files:
-    try:
-        print(file, end='\r')
-        checksum.extend([hashfile(file)])
-    except:
-        print('Cannot acces file.', end='\r')
-        checksum.extend('none')
-
-# Save results to .csv files
-df = pd.DataFrame([full_files, checksum]).T
-df.columns = ['path', 'hash']
-df.set_index('path', inplace=True)
-
-df.to_csv(csv_output)
-
-df.read_csv(csv_output)
-#df.dropna(inplace=True)
-df = df[df.duplicated(['hash'])]
-df.to_csv(csv_output2)
-'''
