f02ffbdfb39858b9483b3db18673eb3aaa78453c
Carsten Klein <>
2020-12-29 14:43:48 +0100

    indent and formatting correction

--- test_detect_duplicates.py
+++ test_detect_duplicates.py
@@ -7,47 +7,51 @@ from Duplicates import Duplicates
 
 class TestDetectDuplicates(unittest.TestCase):
 
-        def setUp(self):
-            self.output_path = 'tmp'
-            self.output_file = '{}/duplicates.csv'.format(self.output_path)
-            self.test_file = '{}/testFile.csv'.format(self.output_path)
-            self.expected_hash = 'c137909ea3e82fc45bc17ccef8c691dc'
+    def setUp(self):
+        self.output_path = 'tmp'
+        self.output_file = '{}/duplicates.csv'.format(self.output_path)
+        self.test_file = '{}/testFile.csv'.format(self.output_path)
+        self.expected_hash = 'c137909ea3e82fc45bc17ccef8c691dc'
 
-            self.duplicates = Duplicates()
+        self.duplicates = Duplicates()
 
-            os.mkdir(self.output_path)
+        os.mkdir(self.output_path)
 
-            df = pd.DataFrame(range(0,5), columns=['test'])
-            df.to_csv(self.test_file)
+        df = pd.DataFrame(range(0, 5), columns=['test'])
+        df.to_csv(self.test_file)
 
-        def tearDown(self):
-            shutil.rmtree(self.output_path)
+    def tearDown(self):
+        shutil.rmtree(self.output_path)
 
-        def test_hash_function(self):
+    def test_hash_method(self):
 
-                self.assertEqual(self.duplicates.hashfile(self.test_file),
-                                 self.expected_hash)
+        self.assertEqual(self.duplicates.hashfile(self.test_file),
+                         self.expected_hash)
 
-        def test_fullfile_function(self):
+    def test_fullfile_without_subfolders(self):
 
-            self.assertEqual(self.duplicates.filelist(self.output_path),
-                             ['tmp\\testFile.csv'])
+        self.assertEqual(self.duplicates.filelist(self.output_path),
+                         ['tmp\\testFile.csv'])
 
-        def test_hashtable_function(self):
+    def test_fullfile_with_subfolders(self):
 
-            inputfile = self.duplicates.filelist(self.output_path)
-            self.assertEqual(self.duplicates.hashtable(inputfile),
-                             [self.expected_hash])
+        print('test')
 
-        def test_detect_dupliacte(self):
+    def test_hashtable_method(self):
 
-            start_file = self.duplicates.filelist(self.output_path)
-            name = start_file[0].split('.csv')
-            duplicatefile = '{}_duplicate.csv'.format(name[0])
+        input_file = self.duplicates.filelist(self.output_path)
+        self.assertEqual(self.duplicates.hashtable(input_file),
+                         [self.expected_hash])
 
-            shutil.copy(start_file[0], duplicatefile)
+    def test_detection_of_duplicates(self):
 
-            inputfiles = self.duplicates.filelist(self.output_path)
-            result = self.duplicates.hashtable(inputfiles)
+        start_file = self.duplicates.filelist(self.output_path)
+        name = start_file[0].split('.csv')
+        duplicate_file = '{}_duplicate.csv'.format(name[0])
 
-            self.assertEqual(result[0], result[1])
+        shutil.copy(start_file[0], duplicate_file)
+
+        input_files = self.duplicates.filelist(self.output_path)
+        result = self.duplicates.hashtable(input_files)
+
+        self.assertEqual(result[0], result[1])
