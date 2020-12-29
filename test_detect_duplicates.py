da835d5e2e4f30a47c45dd4526b9104838c4cfef
Carsten Klein <>
2020-12-29 15:50:15 +0100

    subfolder test added for filelist method (fails)

--- test_detect_duplicates.py
+++ test_detect_duplicates.py
@@ -9,8 +9,8 @@ class TestDetectDuplicates(unittest.TestCase):
 
     def setUp(self):
         self.output_path = 'tmp'
-        self.output_file = '{}/duplicates.csv'.format(self.output_path)
-        self.test_file = '{}/testFile.csv'.format(self.output_path)
+        self.original_file = os.path.join(self.output_path, 'originalFile.csv')
+        self.duplicate_file = os.path.join(self.output_path, 'duplicateFile.csv')
         self.expected_hash = 'c137909ea3e82fc45bc17ccef8c691dc'
 
         self.duplicates = Duplicates()
@@ -18,24 +18,32 @@ class TestDetectDuplicates(unittest.TestCase):
         os.mkdir(self.output_path)
 
         df = pd.DataFrame(range(0, 5), columns=['test'])
-        df.to_csv(self.test_file)
+        df.to_csv(self.original_file)
 
     def tearDown(self):
         shutil.rmtree(self.output_path)
 
     def test_hash_method(self):
 
-        self.assertEqual(self.duplicates.hashfile(self.test_file),
+        self.assertEqual(self.duplicates.hashfile(self.original_file),
                          self.expected_hash)
 
     def test_fullfile_without_subfolders(self):
 
-        self.assertEqual(self.duplicates.filelist(self.output_path),
-                         ['tmp\\testFile.csv'])
+        self.assertEqual(self.duplicates.filelist(self.output_path)[0],
+                         self.original_file)
 
     def test_fullfile_with_subfolders(self):
 
-        print('test')
+        sub_folder = os.path.join(self.output_path, 'sub_folder')
+        os.mkdir(sub_folder)
+
+        sub_folder_file = os.path.join(sub_folder, os.path.split(self.output_file)[1])
+
+        shutil.copy(self.original_file, sub_folder_file)
+
+        self.assertEqual(self.duplicates.filelist(self.output_path),
+                         [self.original_file, sub_folder_file])
 
     def test_hashtable_method(self):
 
@@ -45,11 +53,7 @@ class TestDetectDuplicates(unittest.TestCase):
 
     def test_detection_of_duplicates(self):
 
-        start_file = self.duplicates.filelist(self.output_path)
-        name = start_file[0].split('.csv')
-        duplicate_file = '{}_duplicate.csv'.format(name[0])
-
-        shutil.copy(start_file[0], duplicate_file)
+        shutil.copy(self.original_file, self.duplicate_file)
 
         input_files = self.duplicates.filelist(self.output_path)
         result = self.duplicates.hashtable(input_files)
