4c1343ceca5ac408c580d62876b77f12c66f34ca
akcarsten <>
2021-01-06 23:44:33 +0100

    Release version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='Duplicate-Finder',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/akcarsten/duplicates.git',
     license='MIT License',
     author='Carsten Klein',
