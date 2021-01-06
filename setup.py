8d2f3c068941e6b18c296567ae7c81a87fd2540e
akcarsten <>
2021-01-06 01:59:39 +0100

    bugfixes

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='duplicates',
     version='0.0.1',
-    packages=['tests', 'duplicates'],
     url='https://github.com/akcarsten/duplicates.git',
     license='MIT License',
     author='Carsten Klein',
     author_email='',
     description='Package to find duplicate files in and across folders',
-    packages = setuptools.find_packages(),
-    classifiers = [
+    packages=setuptools.find_packages(),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
