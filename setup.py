f3de1a20119b3b772c42abc486697fa5b39ad106
akcarsten <>
2021-01-06 23:17:45 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='Duplicate-Finder',
-    version='0.0.1',
+    version='1.0.0',
     url='https://github.com/akcarsten/duplicates.git',
     license='MIT License',
     author='Carsten Klein',
     author_email='',
     description='Package to find duplicate files in and across folders',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.8',
 )
